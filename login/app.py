4bbae956477fda798cdcd90e1e3381fe061034cb
Winton Yee <wyee@github.com>
2015-09-29 11:45:12 -0400

    commited project

--- login/app.py
+++ login/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 #When someone goes to "/home", do the thing (must return string).
-@app.route("/home")`
+@app.route("/home")
+@app.route("/")
 def home():
-    return "<h1>Wazzzupppp???</h1>"
+    return render_template("home.html")
 
 #Debug is true to get better error messages
 #Run the app. The host COULD be IP address, but normally put it down as 0.0.0.0 so that anyone can use the app.
