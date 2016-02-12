886e77d72b67f0a00950e27cfef81164952b6966
blehw <winton2095@gmail.com>
2016-02-12 07:22:45 -0500

    dots

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -1,55 +1,43 @@
-//Winton Yee
-//Period 3 SoftDev
-//February 10, 2016
-//HW 1b
+/*Winton Yee
+Period 3 SoftDev
+February 10, 2016
+HW 1a*/
 
-var c = document.getElementById("ftb2maga");
-var ctx = c.getContext("2d");
+var c = document.getElementById("playground");
+var button = document.getElementById("button");
+var ctx=c.getContext("2d");
+ctx.strokeRect(0,0,1000,1000);
 
-//Sets the color that we will color in
-ctx.fillStyle = "#ff0000";
+var x;
+var y;
+var oldX = -1;
+var oldY = -1;
 
-//Fills a rectange, starting at (a,b) and with dimensions cxd
-ctx.fillRect(0,0,100,100);
+function click(e){
+    x=e.pageX;
+    y=e.pageY;
+    ctx.beginPath();
+    ctx.arc(x,y,10,0,2*Math.PI);
+    ctx.fill();
+    ctx.closePath();
+    if (oldX > 0) {
+	ctx.beginPath();
+	ctx.moveTo(oldX,oldY);
+	ctx.lineTo(x,y);
+	ctx.stroke();
+	ctx.closePath();
+    }
+    oldX = x;
+    oldY = y;
 
-//Sets the color that we will draw the outline in
-ctx.strokeStyle = "00ff00";
+}
 
-//Draws the outline of a rectange, starting at (a,b) and with dimensions cxd
-ctx.strokeRect(0,0,100,100);
+function clear(){
+    ctx.clearRect(0,0,playground.width,playground.height);
+    ctx.strokeRect(0,0,1000,1000);
+    oldX = -1
+    oldY = -1
+}
 
-ctx.fillStyle="#00ff00";
-ctx.fillRect(200,0,100,100);
-ctx.strokeStyle="#ff0000";
-ctx.strokeRect(200,0,100,100);
-
-//Marks the current shape we are drawing
-ctx.beginPath();
-
-//Sets the initial point 
-ctx.moveTo(190,100);
-
-//Draws a line connecting this point to beginning one
-ctx.lineTo(190,200);
-ctx.lineTo(100,200);
-ctx.fillStyle ="#0000ff";
-ctx.fill();
-ctx.strokeStyle="#00ff00";
-ctx.stroke();
-
-//Closes our shape
-ctx.closePath();
-
-ctx.beginPath();
-
-//Draws an arc beginning at (a,b) with radius c. d and e????
-ctx.arc(150,300,100,0,200);
-ctx.fillStyle = "#000000";
-ctx.fill();
-ctx.closePath();
-
-//Sets the font and font size
-ctx.font = "30px arial"
-
-//Renders text at coordinates (b,c)
-ctx.fillText("oh! what a cool webpage",0,450);
+playground.addEventListener("click",click);
+button.addEventListener("click",clear);
